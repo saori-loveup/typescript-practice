d65a7bea06cd42bc5294068472057368682ddb14
Saori Ito <saori@itoo.info>
2024-09-13 16:00:31 +0900

    関数の型注釈練習

--- 2024-09-13/typeAnnotations.js
+++ 2024-09-13/typeAnnotations.js
@@ -1,3 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// 変数の型注釈
 var age = 20;
 var gender = "さおり";
-age = "30";
+// age = "30"; // error: Type 'string' is not assignable to type 'number'.
+// 関数の型注釈
+function test(a, b) {
+    return a + b;
+}
+console.log(test(1, 2)); // 引数で渡し型が一致しているので問題なし
+//console.log(test(1, "2")); // error: Argument of type 'string' is not assignable to parameter of type 'number'.
+/* 戻り値が指定の型と異なるエラーの関数
+function test2(a: number): number {
+  return a + ""; // error: Type 'string' is not assignable to type 'number'.
+}
+*/
--- 2024-09-13/typeAnnotations.ts
+++ 2024-09-13/typeAnnotations.ts
@@ -1,4 +1,20 @@
+export {};
+
+// 変数の型注釈
 let age: number = 20;
 const gender: string = "さおり";
 
 // age = "30"; // error: Type 'string' is not assignable to type 'number'.
+
+// 関数の型注釈
+function test(a: number, b: number): number {
+  return a + b;
+}
+console.log(test(1, 2)); // 引数で渡し型が一致しているので問題なし
+//console.log(test(1, "2")); // error: Argument of type 'string' is not assignable to parameter of type 'number'.
+
+/* 戻り値が指定の型と異なるエラーの関数
+function test2(a: number): number {
+  return a + ""; // error: Type 'string' is not assignable to type 'number'. 
+}
+*/
