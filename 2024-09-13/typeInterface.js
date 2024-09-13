b7a8d2ed47f8be4bfa021439b2c0675e81b5ede3
Saori Ito <saori@itoo.info>
2024-09-13 16:33:43 +0900

    型推論の学習

---
+++ 2024-09-13/typeInterface.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var age = 20; // 型推論で変数"age"はNumberとして認識される
+//age = "30"; // error: Type 'string' is not assignable to type 'number'.
+age = 30; // 代入する値がNumberなのでOK
+var gender; // 初期値が設定されていない場合、any型と推論される
+gender = "female"; // OK
+gender = 1; // OK
+function test(a, b) {
+    return a + b; // 戻り値の型はnumberと推論される
+}
---
+++ 2024-09-13/typeInterface.ts
@@ -0,0 +1,13 @@
+export {};
+
+let age = 20; // 型推論で変数"age"はNumberとして認識される
+//age = "30"; // error: Type 'string' is not assignable to type 'number'.
+age = 30; // 代入する値がNumberなのでOK
+
+let gender; // 初期値が設定されていない場合、any型と推論される
+gender = "female"; // OK
+gender = 1; // OK
+
+function test(a: number, b: number) {
+  return a + b; // 戻り値の型はnumberと推論される
+}
