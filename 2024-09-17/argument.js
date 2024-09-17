e9b1f6db0139b3546ede44858b6794d903c2ae72
Saori Ito <saori@itoo.info>
2024-09-17 17:09:18 +0900

    デフォルト値を設定した引数、可変長引数の練習追加

--- 2024-09-17/argument.js
+++ 2024-09-17/argument.js
@@ -12,3 +12,19 @@ function hello(name, title) {
 }
 hello("山田太郎"); // "Hello, 山田太郎!"
 hello("山田太郎", "さん"); // "Hello, 山田太郎 さん!"
+// デフォルト値を設定した引数のある関数
+function greet(name, title = "さん") {
+    console.log(`Hello, ${name} ${title}!`);
+}
+greet("山田太郎"); // "Hello, 山田太郎 さん!"
+greet("山田太郎", "様"); // "Hello, 山田太郎 様!"
+// 可変長引数
+function sum(...numbers) {
+    return numbers.reduce(function (total, num) {
+        return total + num;
+    });
+}
+console.log(sum(1, 2, 3)); // 6
+const numbers = [1, 2, 3];
+console.log(sum(numbers)); // error: Argument of type 'number[]' is not assignable to parameter of type 'number'.
+console.log(sum(...numbers));
--- 2024-09-17/argument.ts
+++ 2024-09-17/argument.ts
@@ -11,3 +11,22 @@ function hello(name: string, title?: string): void {
 }
 hello("山田太郎"); // "Hello, 山田太郎!"
 hello("山田太郎", "さん"); // "Hello, 山田太郎 さん!"
+
+// デフォルト値を設定した引数のある関数
+function greet(name: string, title = "さん"): void {
+  console.log(`Hello, ${name} ${title}!`);
+}
+greet("山田太郎"); // "Hello, 山田太郎 さん!"
+greet("山田太郎", "様"); // "Hello, 山田太郎 様!"
+
+// 可変長引数
+function sum(...numbers: number[]): number {
+  return numbers.reduce(function (total, num) {
+    return total + num;
+  });
+}
+console.log(sum(1, 2, 3)); // 6
+
+const numbers = [1, 2, 3];
+//console.log(sum(numbers)); // error: Argument of type 'number[]' is not assignable to parameter of type 'number'.
+console.log(sum(...numbers)); // 6
